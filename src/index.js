41c74ac6e366a8602dd6a539565fc9817f93a9da
Anna Kowalewskaja <lao7777@ya.ru>
2021-08-14 18:46:58 +0200

    init: start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) == false || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        array.forEach((element) => {
+            if (min > element) {
+                min = element;
+            }
+        });
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) == false || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        array.forEach((element) => {
+            if (max < element) {
+                max = element;
+            }
+        });
 
-exports.max = function max (array) {
-  return 0;
-}
+        return max;
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) == false || array.length === 0) {
+        return 0;
+    } else {
+        let avg = 0;
+        array.forEach((element) => {
+            avg += element;
+        });
+
+        avg /= array.length;
+        return avg;
+    }
+};
